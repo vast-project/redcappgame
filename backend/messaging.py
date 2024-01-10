137bfec5e9e99400400e30a0bb8de1afc12df979
Martin <martin.ruskov@unimi.it>
2024-01-10 15:57:33 +0100

    Added Greek debriefing

--- backend/messaging.py
+++ backend/messaging.py
@@ -1,29 +1,34 @@
 heads = {
     "bg": "Благодарим ви, че участвате в проекта VAST. Вашият принос е много важен за нас.",
     "en": "Thank you for participating in the VAST project. Your contribution is very important to us.",
+    "gr": "Σας ευχαριστούμε για τη συμμετοχή σας στο έργο VAST. Η συνεισφορά σας είναι πολύ σημαντική για εμάς.",
     "it": "Grazie per aver partecipato al progetto VAST. Il tuo contributo è molto importante per noi.",
 }
 
 empties = {
     "bg": "В отговорите си отдавате значение на различни ценности, без някоя от тях да има превес над другите.",
     "en": "In your answers, you gave importance to different values without one being prevalent over the others.",
+    "gr": "Στις απαντήσεις σας, θεωρήσατε σημαντικές διάφορες αξίες από τις οποίες καμία δεν υπερισχύει των άλλων.",
     "it": "Nelle tue risposte hai dato importanza a valori diversi senza che uno sia prevalente rispetto agli altri.",
 }
 
 player_values = {
     "bg": "В отговорите ви преобладава следната ценност: {}",
     "en": "There is a prevalence of the following value in your answers: {}",
+    "gr": "Στις απαντήσεις σας επικρατεί η ακόλουθη αξία: {}",
     "it": "Nelle tue risposte c'è una prevalenza del seguente valore: {}",
 }
 
 counter = {
     "bg": "В това занятие са участвали {} потребители.",
     "en": "The users that participated in this activity are {}.",
+    "gr": "Οι χρήστες που συμμετείχαν σε αυτή τη δραστηριότητα είναι {}.",
     "it": "Hanno partecipato a questa attività {} utenti.",
 }
 
 total_values = {
     "bg": "Събраните до момента отговори показват превес на следната ценност: {}",
     "en": "The answers collected so far show a prevalence of the following value: {}",
+    "gr": "Οι μέχρι στιγμής απαντήσεις δείχνουν ότι επικρατεί η ακόλουθη αξία: {}",
     "it": "Le risposte raccolte fino ad ora mostrano una prevalenza del seguente valore: {}",
 }
